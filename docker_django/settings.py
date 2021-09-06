32ed6f8fa4f8f71cdd54b7613931328282aee450
Wagner Palmeira <wagnerpalmeira.ti@gmail.com>
2021-09-06 08:26:15 -0300

    Secret Key

--- docker_django/settings.py
+++ docker_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0@1odc34_8t2ph950*3^ti!9uv(c250!w*ix5w=jel7bnz_zlf'
+SECRET_KEY = 'GENERATE_YOUR_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
